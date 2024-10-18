c67637d828e416d5dc1c977caa07800e81af5845
Ardit Hyka <xardit@users.noreply.github.com>
2024-10-18 17:34:10 +0200

    auth implentation

--- final_project/index.js
+++ final_project/index.js
@@ -1,22 +1,51 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require('express')
+const jwt = require('jsonwebtoken')
 const session = require('express-session')
-const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
+const { authenticatedUser } = require('./router/auth_users.js')
+const customer_routes = require('./router/auth_users.js').authenticated
+const genl_routes = require('./router/general.js').general
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use('/customer', session({ secret: 'fingerprint_customer', resave: true, saveUninitialized: true }))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
- 
-const PORT =5000;
+app.use('/customer/auth/*', function auth(req, res, next) {
+  //Write the authenication mechanism here
+  const username = req.body.username
+  const password = req.body.password
 
-app.use("/customer", customer_routes);
-app.use("/", genl_routes);
+  // Check if username or password is missing
+  if (!username || !password) {
+    return res.status(404).json({ message: 'Error logging in' })
+  }
 
-app.listen(PORT,()=>console.log("Server is running"));
+  // Authenticate user
+  if (authenticatedUser(username, password)) {
+    // Generate JWT access token
+    let accessToken = jwt.sign(
+      {
+        data: password,
+      },
+      'access',
+      { expiresIn: 60 }
+    )
+
+    // Store access token and username in session
+    req.session.authorization = {
+      accessToken,
+      username,
+    }
+    return res.status(200).send('User successfully logged in')
+  } else {
+    return res.status(208).json({ message: 'Invalid Login. Check username and password' })
+  }
+})
+
+const PORT = 5001
+
+app.use('/customer', customer_routes)
+app.use('/', genl_routes)
+
+app.listen(PORT, () => console.log('Server is running'))
