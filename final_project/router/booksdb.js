348aea4784ec7102d2bd51d2e515664b701d4156
Ardit Hyka <xardit@users.noreply.github.com>
2024-10-18 17:47:49 +0200

    Task 5: Get reviews by isbn

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -6,7 +6,14 @@ let books = {
   5: { author: 'Unknown', title: 'The Book Of Job', reviews: {} },
   6: { author: 'Unknown', title: 'One Thousand and One Nights', reviews: {} },
   7: { author: 'Unknown', title: "Nj\u00e1l's Saga", reviews: {} },
-  8: { author: 'Jane Austen', title: 'Pride and Prejudice', reviews: {} },
+  8: {
+    author: 'Jane Austen',
+    title: 'Pride and Prejudice',
+    reviews: {
+      Joe: 'This book is a good read',
+      Trum: 'I do recommend this book',
+    },
+  },
   9: { author: 'Honor\u00e9 de Balzac', title: 'Le P\u00e8re Goriot', reviews: {} },
   10: { author: 'Samuel Beckett', title: 'Molloy, Malone Dies, The Unnamable, the trilogy', reviews: {} },
 }
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -36,7 +36,7 @@ public_users.get('/title/:title', function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
   //Write your code here
-  return res.status(300).json({ message: 'Yet to be implemented' })
+  return res.status(200).json(books[req.params.isbn].reviews)
 })
 
 module.exports.general = public_users
