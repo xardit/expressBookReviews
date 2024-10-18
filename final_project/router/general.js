37d8494a99e888fe9d9e4a47a0f3867df0195a08
Ardit Hyka <xardit@users.noreply.github.com>
2024-10-18 17:34:53 +0200

    Task 1: Get the book list available in the shop

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,10 +11,10 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/', function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+  return res.status(300).json(books)
+})
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
