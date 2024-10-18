05f1ad06b46266cf20767220ac74d1ea62b00277
Ardit Hyka <xardit@users.noreply.github.com>
2024-10-18 17:52:18 +0200

    Task 6: Register a new user functionality OK

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -8,6 +8,17 @@ let users = []
 const isValid = (username) => {
   //returns boolean
   //write code to check is the username is valid
+  // Check if a user with the given username already exists
+  // Filter the users array for any user with the same username
+  let userswithsamename = users.filter((user) => {
+    return user.username === username
+  })
+  // Return true if any user with the same username is found, otherwise false
+  if (userswithsamename.length > 0) {
+    return true
+  } else {
+    return false
+  }
 }
 
 const authenticatedUser = (username, password) => {
